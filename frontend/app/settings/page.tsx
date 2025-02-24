fe9b946cd1e54a28a2fe04751b17de93dbdf1a00
0x13bbb <0x13rian@gmail.com>
2025-02-24 22:47:48 +1100

    updated settings page for theme

--- frontend/app/settings/page.tsx
+++ frontend/app/settings/page.tsx
@@ -1,9 +1,19 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 export default function Settings() {
     return (
-        <>
-            <h2 className="m-8 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-                TODO: settings for notifier
+        <div className="m-8">
+            <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                settings
             </h2>
-        </>
+            <div className="flex items-center">
+                <p className="mr-4">theme: </p>
+                <ModeToggle/>
+            </div>
+            <div className="flex items-center">
+                <p className="mr-4">TODO: settings for notifier</p>
+            </div>
+            
+        </div>
     )
 }
