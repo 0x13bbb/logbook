cc545540f7797d839bacae0d381ecb8cfd9ad052
0x13bbb <0x13rian@gmail.com>
2025-02-24 20:49:36 +1100

    init settings page

---
+++ frontend/app/settings/page.tsx
@@ -0,0 +1,9 @@
+export default function Settings() {
+    return (
+        <>
+            <h2 className="m-8 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                TODO: settings for notifier
+            </h2>
+        </>
+    )
+}
