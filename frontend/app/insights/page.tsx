da224fabf4b0c771044e17f2186bc6a43e527b1e
0x13bbb <0x13rian@gmail.com>
2025-02-24 22:45:41 +1100

    added example chart

--- frontend/app/insights/page.tsx
+++ frontend/app/insights/page.tsx
@@ -1,9 +1,12 @@
+import { ExampleChart } from "@/components/example-chart";
+
 export default function Insights() {
   return (
-    <>
-        <h2 className="m-8 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <div className="m-8">
+        <h2 className="mb-4 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
             TODO: insights @emleda
         </h2>
-    </>
+        <ExampleChart/>
+    </div>
   )
 }
