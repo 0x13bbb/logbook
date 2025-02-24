3b0fccd0ef428a455be3cc0b7837001593e1f638
0x13bbb <0x13rian@gmail.com>
2025-02-24 20:48:59 +1100

    init insights for @emleda

---
+++ frontend/app/insights/page.tsx
@@ -0,0 +1,9 @@
+export default function Insights() {
+  return (
+    <>
+        <h2 className="m-8 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            TODO: insights @emleda
+        </h2>
+    </>
+  )
+}
